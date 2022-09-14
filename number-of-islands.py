b8c251a89e89ec76b575a8f673f7682bf4cd0357
mvallido <vallido.michael@gmail.com>
2022-09-14 11:36:31 -0700

    Built dfs algo to find the largest island in the given graph

--- number-of-islands.py
+++ number-of-islands.py
@@ -1,10 +1,30 @@
+def totalIslandArea(graph) -> int:
+    row, col = len(graph), len(graph[0])
+    visited = set()
+    largestIsland = 0
+    
+    def dfs(r,c):
+        if r not in range(row) or c not in range(col) or graph[r][c] == 0 or (r,c) in visited:
+            return 0
+        
+        visited.add((r,c))
+        return 1 + dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
+
+    
+    for r in range(row):
+        for c in range(col):
+            if (r,c) not in visited and graph[r][c] == 1:
+                largestIsland = max(largestIsland, dfs(r,c))
+    
+    return largestIsland
+
 def numberOfIslands(graph) -> int: 
     row, col = len(graph), len(graph[0])
     visited = set()
     count = 0
     
     def dfs(r,c):
-        if r not in range(row) or c not in range(col) or graph[r][c] == "0" or (r,c) in visited:
+        if r not in range(row) or c not in range(col) or graph[r][c] == 0 or (r,c) in visited:
             return
         
         visited.add((r,c))
@@ -16,8 +36,28 @@ def numberOfIslands(graph) -> int:
     
     for r in range(row):
         for c in range(col):
-            if (r,c) not in visited and graph[r][c] == "1":
+            if (r,c) not in visited and graph[r][c] == 1:
                 dfs(r,c)
                 count += 1
     
     return count
+
+graph = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+         [0,0,0,0,0,0,0,1,1,1,0,0,0],
+         [0,1,1,0,1,0,0,0,0,0,0,0,0],
+         [0,1,0,0,1,1,0,0,1,0,1,0,0],
+         [0,1,0,0,1,1,0,0,1,1,1,0,0],
+         [0,0,0,0,0,0,0,0,0,0,1,0,0],
+         [0,0,0,0,0,0,0,1,1,1,0,0,0],
+         [0,0,0,0,0,0,0,1,1,0,0,0,0],
+         [0,1,0,1,0,0,0,1,1,0,0,0,1]]
+
+def init(graph) -> str:
+    islandCount = numberOfIslands(graph)
+    largestIsland = totalIslandArea(graph)
+    
+    print(f"""The total number of islands in this graph is {islandCount}
+        and the largest island has an area of {largestIsland}
+        """)
+
+init(graph)
